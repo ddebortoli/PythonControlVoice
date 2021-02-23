077d2e712b08e7769c0cb6915019997268a2ec52
ddebortoli <damiandebortolilb@gmail.com>
2021-02-22 21:52:52 -0300

    Servicio terminado y funcional

--- index.py
+++ index.py
@@ -1,12 +1,8 @@
 import speech_recognition as sr
 import webbrowser
-
-
-listOfWebSites = {
-        'ABRIR GOOGLE':'https://www.google.com/',
-        'ABRIR FACEBOOK': 'https://www.facebook.com/',
-        'ABRIR INSTAGRAM': 'https://www.instagram.com/'
-        }
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
 
 def autoexec():
     "Funcion que obtiene un comando de voz dictado por el usuario"
@@ -23,29 +19,51 @@ def autoexec():
             text = 'vacio'
     return text
 
+def findInList(command,show):
+    "Funcion que muestra el listado de comandos u obtiene un comando existente"
+    commands = pd.read_excel('Datos/Comandos.xlsx', sheet_name='ListadoDeWebs')
+    for i in commands.index:
+        if show:
+            print(commands['COMANDO'][i] + "|" + commands['URL'][i])
+        elif command == commands['COMANDO'][i]:
+            return commands['URL'][i]
+    return None    
+
+def addNewCommand(command,url):
+    "Funcion que inserta datos en el excel"
+    
+    commands = pd.read_excel('Datos/Comandos.xlsx', sheet_name='ListadoDeWebs')
+    
+    if findInList(command,False) != None:
+        print("Error! El comando ya existe.")
+    else:
+        try:
+            commands = commands.append({'COMANDO':command,'URL':url},ignore_index=True)
+            print(commands)
+            commands.to_excel("Datos/Comandos.xlsx",sheet_name='ListadoDeWebs')
+        except:
+            print("Error al guardar, intente nueveamente.")
 
 def runCommand(command):
     "Funcion que ejecuta un comando"
-    if command == 'AGREGAR WEB':
+    
+    if command == 'AGREGAR WEB': #Si el comando es 'Agregar WEB' ejecuta la funcion addNewCommand
         print("Ingrese la sintaxis del comando:")
         newCommand = input()
         print("Ingrese la web a la que desea anexar este comando:")
         newUrl = input()
-        addItem = listOfWebSites.setdefault(newCommand.upper(),newUrl)
+        addNewCommand(newCommand.upper(),newUrl)
         
-        print("{} agregado exitosamente con el comando!" .format(listOfWebSites[newCommand]))      
-    elif listOfWebSites.get(command) != None:
-        webbrowser.open(listOfWebSites.get(command) , new=2, autoraise=True)
-    elif command == 'MOSTRAR LISTA':
-        for key in listOfWebSites:
-            print (key, ":", listOfWebSites[key])
+    elif findInList(command,False) != None: #Si el comando dictado por el usuario esta en la lista, lo ejecuta
+        webbrowser.open(findInList(command,False) , new=2, autoraise=True)
+    elif command == 'MOSTRAR LISTA': #Muestra la lista de comandos
+        findInList('pass',True)
         
-    elif command == 'CERRAR PROGRAMA':
+    elif command == 'CERRAR PROGRAMA': #Termina el programa
         return False
     else:
         print("El comando dictado no corresponde con el listado disponible")
     return True
-   #webbrowser.open("https://www.instagram.com/", new=2, autoraise=True)
 
 keepRuning = True
 while(keepRuning):        
